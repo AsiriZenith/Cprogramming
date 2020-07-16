c07d5f1cd03667c9513b61bb4aa7719172e670c5
Asiri Senith <asirisenith@gmail.com>
2020-07-16 11:29:22 +0530

    created four type of insert functions in linklist_test.c file

--- linklist_test1.c
+++ linklist_test1.c
@@ -12,7 +12,9 @@ struct node *createList(struct node *);
 struct node *display(struct node *);
 struct node *insert_beg(struct node *);
 struct node *insert_end(struct node *);
-//struct node *insert_before(struct node *);
+struct node *insert_before(struct node *);
+struct node *insert_after(struct node *);
+//struct node *delete_beg(struct node *);
 
 int main()
 {
@@ -23,7 +25,8 @@ int main()
     insert_end(start);
     display(start);
     printf("\nHello World");
-
+    insert_before(start);
+    insert_after(start);
     return 0;
 }
 
@@ -109,5 +112,57 @@ struct node *insert_end(struct node *start)
         ptr = ptr->next;
     }
     ptr->next = new;
+    return start;
+}
+
+struct node *insert_before(struct node *start)
+{
+    struct node *new, *ptr1, *ptr2;
+    int num, val;
+    printf("\nEnter the data: ");
+    scanf("%d", &num);
+    printf("\nEnter the value before which the data has to be inserted : ");
+    scanf("%d", &val);
+    printf("\nBefore insert: ");
+    display(start);
+    new = (struct node *)malloc(sizeof(struct node));
+    new->data = num;
+    ptr1 = start;
+    //ptr2 = ptr1;
+    while (ptr1->data != val)
+    {
+        ptr2 = ptr1;
+        ptr1 = ptr1->next;
+    }
+    ptr2->next = new;
+    new->next = ptr1;
+    printf("\nafter insert: ");
+    display(start);
+    return start;
+}
+
+struct node *insert_after(struct node *start)
+{
+    struct node *new, *ptr1, *ptr2;
+    int num, val;
+    printf("\nEnter the data: ");
+    scanf("%d", &num);
+    printf("\nEnter the value after which the data has to be inserted : ");
+    scanf("%d", &val);
+    printf("\nBefore insert: ");
+    display(start);
+    new = (struct node *)malloc(sizeof(struct node));
+    new->data = num;
+    ptr1 = start;
+    ptr2 = ptr1;
+    while (ptr2->data != val)
+    {
+        ptr2 = ptr1;
+        ptr1 = ptr1->next;
+    }
+    ptr2->next = new;
+    new->next = ptr1;
+    printf("\nafter insert: ");
+    display(start);
     return start;
 }
